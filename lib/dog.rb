885249356875c6fb174a306032924f722cefe6f6
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-27 23:42:11 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(dog_name)
+    @name_dog = dog_name
+  end 
+  
+  def breed=(dog_breed)
+    @my_breed = dog_breed
+  end 
+  
+  def name 
+    @name_dog
+  end 
+  
+  def breed
+    @my_breed
+  end 
+end 

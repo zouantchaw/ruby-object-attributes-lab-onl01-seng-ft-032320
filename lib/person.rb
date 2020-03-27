38e76d5894476329de851135ee5887b204ea11de
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-27 23:46:40 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(my_name)
+    @name = my_name
+  end 
+  
+  def job=(my_job)
+    @job = my_job
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def job 
+    @job
+  end 
+end 
